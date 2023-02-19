50a0f0dc8eb674bb7e727fa8f9a52aeb9ee37f2d
jsbento <benton_jacob@yahoo.com>
2023-02-19 17:26:47 -0600

    demo chat bot using sockets from gorilla/websocket examples

--- main.go
+++ main.go
@@ -1,38 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
 	"net/http"
 
-	ws "github.com/gorilla/websocket"
+	"github.com/jsbento/game-hub/chat"
 )
 
-var upgrader = ws.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
+var addr = flag.String("addr", ":8080", "http service address")
 
 func main() {
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			fmt.Println(err)
+	flag.Parse()
+	hub := chat.NewHub()
+	go hub.Run()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL)
+		if r.URL.Path != "/" {
+			http.Error(w, "Not found", http.StatusNotFound)
 			return
 		}
-		for {
-			mt, message, err := conn.ReadMessage()
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), message)
-
-			if err := conn.WriteMessage(mt, message); err != nil {
-				fmt.Println(err)
-				return
-			}
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
 		}
+		http.ServeFile(w, r, "home.html")
 	})
-
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/ws-chat", func(w http.ResponseWriter, r *http.Request) {
+		chat.ServeWs(hub, w, r)
+	})
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
