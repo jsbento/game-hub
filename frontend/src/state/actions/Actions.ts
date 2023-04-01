b63fb428887882a8cf5e7307da366a1c711c8ae3
jsbento <benton_jacob@yahoo.com>
2023-04-01 14:34:02 -0500

    fix full stack auth process

--- frontend/src/state/actions/Actions.ts
+++ frontend/src/state/actions/Actions.ts
@@ -1,4 +1,6 @@
-import { Token, User } from "../../types/Users";
+import { Token, User, UserWithToken } from "../../types/Users";
 
 export const setUser = (payload: User | null) => ({ type: "SET_USER", payload });
 export const setToken = (payload: Token | null) => ({ type: "SET_TOKEN", payload });
+export const setUserToken = (payload: UserWithToken | null) => ({ type: "SET_USER_TOKEN", payload });
+export const clearState = () => ({ type: "CLEAR_STATE" });
